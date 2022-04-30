4f80e79e432cd8c3f8d683cd785966abff9cf148
abd <abd@debby.debby>
2022-04-30 06:39:22 +0300

    KDV added

---
+++ KDV.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class KDV {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        /**
+         *  KDV'siz Fiyat = 10;
+            KDV'li Fiyat = 11.8;
+            KDV tutarı = 1.8;
+
+        Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 ,
+        tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
+         */
+
+        int amount = scan.nextInt();
+        int VAT;
+        System.out.println("KDV'siz Fiyat = " + amount);
+        if (amount>1000) {
+            VAT = (amount/100)*8;
+        }else{
+            VAT = (amount/100)*18;
+        }
+        System.out.println("KDV'li Fiyat = " + (amount + VAT));
+        System.out.println("KDV tutari = " + VAT);
+        
+        scan.close();
+    }    
+}
