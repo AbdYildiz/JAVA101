a5f07563d08f1769b072fc7ee8499a2821fbb8df
abd <abd@debby.debby>
2022-05-01 11:49:53 +0300

    Ucgenin alani

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## NotOrtalamasi
 Dersleri toplayip ders sayisina bolerek not ortalamasini alan program# test-repo
+
+## KDV Tutari
+Urun fiyatina KDV oranini ekleyen program
---
+++ UcgeninAlani/Alan.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Alan{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ucgenin kenar uzunluklarini sirasi ile giriniz: ");
+        /**
+        Üç𝑔𝑒𝑛𝑖𝑛 ç𝑒𝑣𝑟𝑒𝑠𝑖 = 2𝑢
+        Alan * Alan = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)
+         */
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        int u = (a+b+c)/2; //cevrenin yarisi
+        double alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        
+        System.out.println("Ucgenin alani = "+ alan);
+        scan.close();
+    }
+}
