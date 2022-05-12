37d8a39277543c0e39576c1fa37f5be0d91c0377
Rogue6one <abdullah-yildiz61@hotmail.com>
2022-05-12 03:20:49 +0300

    Harmonik added

---
+++ Harmonik/Harmonik.java
@@ -0,0 +1,20 @@
+package JAVA101.Harmonik;
+
+import java.util.Scanner;
+
+public class Harmonik {
+    public static void main(String[] args) {
+        /**Java ile girilen sayının harmonik serisini bulan program yazacağız. */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("N sayisini giriniz:");
+        int n = scan.nextInt();
+        double total = 0;
+
+        for (double i = 1; i <= n; i++) {
+            total += (1/i) ;
+        }
+
+        System.out.println(total);
+        scan.close();
+    }
+}
